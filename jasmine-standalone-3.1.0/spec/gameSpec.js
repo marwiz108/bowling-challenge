b3039b3b6c6aa2f581fbc437743cef4c9679955e
Marwa Elali <marwaelali@gmail.com>
2018-04-29 13:27:16 +0100

    went back and changed tests to account for 2 rolls per frame

--- jasmine-standalone-3.1.0/spec/gameSpec.js
+++ jasmine-standalone-3.1.0/spec/gameSpec.js
@@ -8,30 +8,37 @@ describe("Bowling Game", function () {
     game = new Game();
   });
 
-  it("can roll and push to rolls array", function() {
+  it("can roll and store the pins in an array", function() {
     game.roll(3);
     game.roll(2);
     expect(game.framescore()).toEqual([3, 2]);
   });
 
-  it("can roll all gutter plays", function() {
-    for(var i = 0; i < 20; i++) {
-      game.roll(0);
-    };
-
-    console.log(game.framescore());
-
-    expect(game.score()).toBe(0);
+  it("does not allow more than 2 rolls per frame", function() {
+    game.roll(3);
+    game.roll(4);
+    game.roll(1);
+    expect(game.framescore()).toEqual([3, 4]);
   });
 
-  it("can roll all ones", function() {
-    for(var i = 0; i < 20; i++) {
-      game.roll(1);
-    };
+  // it("can roll all gutter plays", function() {
+  //   for(var i = 0; i < 20; i++) {
+  //     game.roll(0);
+  //   };
+  //
+  //   expect(game.score()).toBe(0);
+  // });
+  //
+  // it("can roll all ones", function() {
+  //   for(var i = 0; i < 20; i++) {
+  //     game.roll(1);
+  //   };
+  //
+  //   expect(game.score()).toBe(20);
+  // });
 
-    console.log(game.framescore());
-
-    expect(game.score()).toBe(20);
-  });
+  // it("has 2 rolls per frame", function() {
+  //
+  // });
 
 });
--- jasmine-standalone-3.1.0/src/game.js
+++ jasmine-standalone-3.1.0/src/game.js
@@ -2,20 +2,23 @@
 
 function Game() {
   this.rolls = [];
+  this.MAX_ROLLS = 2;
   this.currentScore = 0;
 };
 
 Game.prototype.roll = function(pins) {
-  (this.rolls).push(pins);
+  if (this.rolls.length < this.MAX_ROLLS) {
+    (this.rolls).push(pins);
+  };
 };
 
 Game.prototype.framescore = function() {
   return this.rolls;
 };
 
-Game.prototype.score = function() {
-  for (var i = 0; i < 20; i++) {
-    this.currentScore += this.rolls[i];
-  };
-  return this.currentScore;
-};
+// Game.prototype.score = function() {
+//   for (var i = 0; i < 20; i++) {
+//     this.currentScore += this.rolls[i];
+//   };
+//   return this.currentScore;
+// };
